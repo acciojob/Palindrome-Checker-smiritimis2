79da02b78cf2c233ad858d4b98a398d5f6208289
smiritimis2 <113242412+smiritimis2@users.noreply.github.com>
2023-01-31 13:11:37 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,63 @@
 // complete the given function
 
-function palindrome(str){
+/*function palindrome(str){
 
 }
+module.exports = palindrome*/
+function palindrome(str) {
+
+    str = str.toLowerCase();
+
+    // Using Spread operator on string
+
+    let arr = [...str];
+
+    // console.log(arr);
+
+    let arrCopy = [...str];    // as reverse method also make changes to original array
+
+    let arrReverse = arr.reverse();
+
+        arr = arrCopy;
+
+    console.log(`reverseArray:${arrReverse}`);
+
+    console.log(`array:${arr}`);
+
+ 
+
+     for (let i = 0; i < arr.length; i++) {
+
+        if (arrCopy[i] != arrReverse[i]) { 
+
+            return false;
+
+        }
+
+    }
+
+ 
+
+    // Directly looping over a string
+
+    // var n = str.length;
+
+    // for (let i = 0; i < n / 2; i++) {
+
+    //     if (str[i] != str[n - i - 1]) { 
+
+    //         return false;
+
+    //     }
+
+    // }
+
+    return true;
+
+}
+
+ 
+
+console.log(palindrome("abc"))
+
 module.exports = palindrome
